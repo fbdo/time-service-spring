25aa43e783911328bfa1d324898d38d7b08d16f6
Fabio Oliveira (FG-233) <fabio.oliveira@bmw.de>
2018-10-30 12:08:24 +0100

    Externalized configuration

--- src/main/java/com/bmwgroup/timeservice/adapters/worldtime/WorldTimeClient.java
+++ src/main/java/com/bmwgroup/timeservice/adapters/worldtime/WorldTimeClient.java
@@ -2,16 +2,21 @@ package com.bmwgroup.timeservice.adapters.worldtime;
 
 import com.bmwgroup.timeservice.application.TimeClient;
 import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WorldTimeClient implements TimeClient {
+
+    @Value("worldtime.url")
+    private String wordTimeURL;
+
     @Override
     public DateTime now() {
         RestTemplate restTemplate = new RestTemplate();
 
-        WorldTime wt = restTemplate.getForObject("http://worldtimeapi.org/api/ip", WorldTime.class);
+        WorldTime wt = restTemplate.getForObject(wordTimeURL, WorldTime.class);
 
         return DateTime.parse(wt.getDatetime());
     }
