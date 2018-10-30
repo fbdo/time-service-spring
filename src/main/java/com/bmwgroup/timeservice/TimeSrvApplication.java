bd7493e51c021a3b0ad6380243e675353e42f848
Fabio Oliveira (FG-233) <fabio.oliveira@bmw.de>
2018-10-30 11:56:30 +0100

    Add circuit break config and implementation

--- src/main/java/com/bmwgroup/timeservice/TimeSrvApplication.java
+++ src/main/java/com/bmwgroup/timeservice/TimeSrvApplication.java
@@ -2,8 +2,10 @@ package com.bmwgroup.timeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class TimeSrvApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bmwgroup/timeservice/application/TimeService.java
+++ src/main/java/com/bmwgroup/timeservice/application/TimeService.java
@@ -1,6 +1,7 @@
 package com.bmwgroup.timeservice.application;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,12 @@ public class TimeService {
     @Autowired
     private TimeClient client;
 
+    @HystrixCommand(fallbackMethod = "reliableNow")
     public DateTime now() {
         return client.now();
+    }
+
+    public DateTime reliableNow() {
+        return DateTime.now();
     }
 }
